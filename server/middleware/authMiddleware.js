085100d59cc5b77ec79ca2e7ec0ca553859a2acd
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-09-25 21:28:53 +0600

    auth middleware validation

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,7 +5,10 @@ const { verifytoken } = require("../services/helpers")
 
         try {  const token = req.cookies["X-AS-Token"]
  
+    if(!token) return res.status(401).send({message : "Invalid Request"})
+    const decoded = verifytoken(token)
 
+    if(!decoded) return res.status(401).send({message : "Invalid Request"})
   
         req.user =decoded
         next()
